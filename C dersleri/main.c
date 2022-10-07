4e6e27fdd59f7263e1e022a28febc03412ddd965
alperaktug1579 <aktugalperen9@gmail.com>
2022-10-07 07:57:40 +0300

    ilk gönderim

---
+++ C dersleri/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	char kitapadi[20] ="mufettis" ;
+	char kitapyazar[10]="gogol";
+	
+	printf("%s\n" , kitapadi);
+	printf("%s" , kitapyazar);
+	
+	return 0;
+}
---
+++ C dersleri/performans1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	int sayi1 , sayi2 , toplam ;
+	 sayi1 = 20 ;
+	 sayi2 = 30 ;
+	toplam = sayi1 + sayi2 ;
+	printf("iki sayinin toplami :  %d" ,  toplam );
+	
+
+	return 0;
+}
