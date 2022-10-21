ffa4e847d047160c1c6ab50ff1ef71957236017b
alperaktug1579 <aktugalperen9@gmail.com>
2022-10-21 15:06:01 +0300

    ilk gönderim

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -5,12 +5,83 @@ var router =express.Router();
 
 /* GET home page. */
    const anaSayfa = function(req, res, next) {
-    res.render('anasayfa' , {'title': 'Anasayfa'});
-    
-  };
+    res.render('anasayfa' ,
+     {
+      "baslik": "Anasayfa",
+      "sayfaBaslik": {
+        "siteAd":"Mekanbul",
+        "slogan":"Civardaki Mekanları Keşfet!"
 
+      },
+      "mekanlar":[
+        {
+          "ad":"Starbucks",
+          "adres":"Centrum Garden AVM",
+          "puan": "4",
+          "imkanlar":["Dünya  Kahveleri","Kekler","Pastalar"],
+          "mesafe": "10km"
+        }
+        ,
+        {
+          "ad":"Gloria  Jeans",
+          "adres":"Sdü  Doğu  Kampüsü",
+          "puan": "3",
+          "imkanlar":["Kahve","Çay","Pasta"],
+          "mesafe": "5km"
+        }
+        
+
+      ]
+    
+  }
+  );
+   }
   const mekanBilgisi= function(req, res, next) {
-    res.render('mekanbilgisi' , {'title': 'Mekan Bilgisi'});
+    res.render('mekanbilgisi' ,
+     {
+    "baslik": "Mekan Bilgisi",
+     "mekanBaslik":"Starbucks",
+     "mekanDetay":{
+      "ad":"Starbucks",
+      "puan":"3",
+      "adres":"Centruam Garden AVM",
+      "imkanlar":["Kahve","Çay","Kek"],
+      "koordinatlar":{
+        "enlem":"37.7",
+        "boylam":"30.5"
+      },
+      "saatler":[
+        {
+          "gunler":"Pazartesi-Cuma",
+          "acilis":"9:00",
+          "kapanis":"23:00",
+          "kapali": false
+        },
+        {
+          "gunler":"Cumartesi-Pazar",
+          "acilis":"10:00",
+          "kapanis":"22:00",
+          "kapali": false
+        }
+        
+      ],
+      
+      "yorumlar":[
+        {
+          "yorumYapan":"Alperen  Aktuğ",
+          "yorumMetni":"berbatt",
+          "tarih":"20 Ekim  2022",
+          "puan":"5"
+
+          
+        }
+      ]
+
+      
+
+     }
+    
+    });
     
   };
 
@@ -23,4 +94,4 @@ var router =express.Router();
     anaSayfa,
     mekanBilgisi,
     yorumEkle
-  }
+  };
